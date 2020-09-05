52c231fe5acd26d6313d3f9f96012ff7dcfda027
Felipe Honorato <felipehonoratods@gmail.com>
2020-09-05 13:10:27 -0300

    build: creation of mobile functionality

---
+++ huntapp/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Routes from './src/routes';
+
+import './src/config/statusBarConfig';
+
+console.disableYellowBox = true;
+
+const App = () => <Routes />;
+
+export default App;
